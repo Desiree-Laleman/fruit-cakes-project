86ec34f8d7eef0c8e1b77b527b1baee0883fcfab
Byron Lee Jr <leebyron120@gmail.com>
2023-08-24 13:22:23 -0400

    got the bare bones in

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
 import "./App.css";
+import Cake from "./components/Cake";
+import CakeBuilder from "./components/CakeBuilder";
+import Layer from "./model/Layer";
 
 function App() {
-  return <div className="App"></div>;
+  const [cakeArray, setCakeArray] = useState<Layer[]>([]);
+  return (
+    <div className="App">
+      <Cake cakeArray={cakeArray} />
+      <CakeBuilder />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Cake.tsx
@@ -0,0 +1,17 @@
+import Layer from "../model/Layer";
+import "./Cake.css";
+import CakeLayer from "./CakeLayer";
+
+interface Props {
+  cakeArray: Layer[];
+}
+
+const Cake = ({ cakeArray }: Props) => {
+  return (
+    <div className="Cake">
+      <CakeLayer />
+    </div>
+  );
+};
+
+export default Cake;
